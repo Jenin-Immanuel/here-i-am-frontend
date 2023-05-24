58fce4a6df61250c39118fea135e69093b23cf64
Jenin <jeninimmi2003@gmail.com>
2023-05-24 09:22:46 +0530

    Modified frontend   styles

---
+++ src/js/indexpage.js
@@ -0,0 +1,24 @@
+const getCookie = (cname) => {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+};
+
+document.addEventListener("load", () => {
+  const cookie = getCookie("accessToken");
+  console.log(cookie);
+  if (cookie) {
+    window.location.pathname = "/landingN.html";
+    console.log("Hello");
+  }
+});
